b575c7638e62645810f139b6dd6b0c6e95a28dc1
Pedro Lacerda <pedroagml@al.insper.edu.br>
2021-05-09 19:06:12 -0300

    Nada mudou. Só botei uma mensagem.

--- cria_baralho.py
+++ cria_baralho.py
@@ -1,4 +1,4 @@
-# Função que cria e embaralha as cartas #
+# Esta função apenas cria o baralho. Outra função estará encarregada de mostra-lo no terminal
 
 def cria_baralho():
     import random
@@ -6,31 +6,6 @@ def cria_baralho():
     '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♥',
     '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♦',
     '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♣']
-    cartas_coloridas = []
-    for c in cartas:
-        if "♠" in c:
-            col = "\033[1;30;40m {0}"
-            cartas_coloridas.append(col.format(c))
-        elif "♥" in c:
-            col = "\033[1;31;40m {0}"
-            cartas_coloridas.append(col.format(c))
-        elif "♦" in c:
-            col = "\033[1;31;40m {0}"
-            cartas_coloridas.append(col.format(c))
-        elif "♣" in c:
-            col = "\033[1;30;40m {0}"
-            cartas_coloridas.append(col.format(c))
-
-    random.shuffle(cartas_coloridas)
     
-    for item in cartas_coloridas:
-        ic = cartas_coloridas.index(item)
-        if ic <= 9:
-            ind_cartas = ("\033[0;37;40m  {0}".format(ic))
-            print("{0}. ".format(ind_cartas) + item)
-        else:
-            ind_cartas = ("\033[0;37;40m {0}".format(ic))
-            print("{0}. ".format(ind_cartas) + item)
-    
-
-print(cria_baralho())
+    random.shuffle(cartas)
+    return cartas
