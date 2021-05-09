093a52bd96489244800e30e231c738ee4b0c3a86
Pedro Lacerda <pedroagml@al.insper.edu.br>
2021-05-09 18:27:04 -0300

    Pequeno ajuste na formatação dos indices. Caso o outro commit não tenha sido enviado, eu botei as cores das cartas de acordo com o naipe

--- cria_baralho.py
+++ cria_baralho.py
@@ -6,6 +6,31 @@ def cria_baralho():
     '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♥',
     '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♦',
     '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♣']
+    cartas_coloridas = []
+    for c in cartas:
+        if "♠" in c:
+            col = "\033[1;30;40m {0}"
+            cartas_coloridas.append(col.format(c))
+        elif "♥" in c:
+            col = "\033[1;31;40m {0}"
+            cartas_coloridas.append(col.format(c))
+        elif "♦" in c:
+            col = "\033[1;31;40m {0}"
+            cartas_coloridas.append(col.format(c))
+        elif "♣" in c:
+            col = "\033[1;30;40m {0}"
+            cartas_coloridas.append(col.format(c))
+
+    random.shuffle(cartas_coloridas)
     
-    random.shuffle(cartas)
-    return cartas
+    for item in cartas_coloridas:
+        ic = cartas_coloridas.index(item)
+        if ic <= 9:
+            ind_cartas = ("\033[0;37;40m  {0}".format(ic))
+            print("{0}. ".format(ind_cartas) + item)
+        else:
+            ind_cartas = ("\033[0;37;40m {0}".format(ic))
+            print("{0}. ".format(ind_cartas) + item)
+    
+
+print(cria_baralho())
