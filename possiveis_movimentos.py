c8b4162128407f3cb1031a210bcbbf7e774bb108
Pedro Lacerda <pedroagml@al.insper.edu.br>
2021-05-08 21:55:30 -0300

    A função não funcionava para baralhos com mais de 4 cartas ou com cartas de valor 10. Corigir foi um pesadelo, mas essa versão passou no teste do python academy.

--- possiveis_movimentos.py
+++ possiveis_movimentos.py
@@ -1,40 +1,61 @@
-def lista_movimetos_possiveis(baralho, indice):
-    c_sel = baralho[indice]
-    if indice == 0:
-        return []
-    elif indice == 1:
-        if c_sel[0] == baralho[0][0]:
-            # msm valor:
-            return [1]
-        elif c_sel[1] == baralho[0][1]:
-            # msm naipe:
-            return [1]
-        else:
-            return []
+def lista_movimentos_possiveis(baralho, index):
+    c_sel = baralho[index]
 
-    elif indice == 2:
-        if c_sel[0] == baralho[1][0]:
+    if index == 0:
+        return []
+    elif index <= 2:
+        if len(c_sel) == 3:
+            if "10" in baralho[index - 1]:
+                # carta anterior é 10
+                return [1]
+            elif baralho[index - 1][-1] == c_sel[-1]:
+                # msm naipe da anterior
+                return [1]
+            else:
+                return []
+
+        if baralho[index - 1][0] == c_sel[0]:
             # msm valor
             return [1]
-        elif c_sel[1] == baralho[1][1]:
+        elif baralho[index - 1][1] == c_sel[1]:
             # msm naipe
             return [1]
         else:
             return []
 
-    elif indice == 3:
-        l = []
-        if c_sel[0] == baralho[2][0]:
-            # msm valor do vizinho anterior
-            l.append(1)
-        elif c_sel[0] == baralho[0][0]:
-            # msm valor do 3º vizinho anterior
-            l.append(1)
-        if c_sel[1] == baralho[2][1]:
-            # msm naipe do vizinho anterior
-            l.append(3)
-        elif c_sel[1] == baralho[0][1]:
-            # msm naipe do 3º vizinho anterior     
-            l.append(3)  
+    elif index > 2:
+        p_mov = []
+        if len(c_sel) == 3:
+            if "10" in baralho[index - 1]:
+                # msm valor 1
+                p_mov.append(1)
+            elif baralho[index - 1][-1] == c_sel[-1]:
+                # msm naipe 1
+                p_mov.append(1)
 
-        return l
+            if "10" in baralho[index - 3]:
+                # msm valor 3
+                p_mov.append(3)
+            elif baralho[index - 3][-1] == c_sel[-1]:
+                # msm naipe 3
+                p_mov.append(3)
+
+            return p_mov
+
+        elif baralho[index - 1][0] == c_sel[0]:
+            # msm valor 1
+            p_mov.append(1)
+        elif baralho[index - 1][1] == c_sel[1]:
+            # msm naipe 1
+            p_mov.append(1)
+
+        if baralho[index - 3][0] == c_sel[0]:
+            # msm valor 3
+            p_mov.append(3)
+        elif baralho[index - 3][1] == c_sel[1]:
+            # ms naipe 3
+            p_mov.append(3)
+        else: 
+            return p_mov
+
+        return p_mov
