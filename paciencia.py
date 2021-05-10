4f6574c3d18e59e5f4d05b3d96b6d7d4d606d1f1
Pedro Lacerda <pedroagml@al.insper.edu.br>
2021-05-10 15:05:00 -0300

    Montando o jogo final. Falta dar cor às cartas e dar as decisões ao jogador.

--- paciencia.py
+++ paciencia.py
@@ -1,52 +1,50 @@
-# Importa as funções do jogo:
-from cria_baralho import cria_baralho
-from extrai_naipe import extrai_naipe
-from extrai_valor import extrai_valor
-from possiveis_movimentos import lista_movimentos_possiveis
-from empilha import empilha
-from possui_movimentos import possui_movimentos_possiveis
+# Novo plano é tentar fazer o jogo funcionar antes de implementar as coisasque fazem ele ficar bonito.
 
-deck_do_jogo = cria_baralho()
+# Importa as bibliotecas necessárias:
+from cria_baralho import cria_baralho as cb # funcionando; args=(), retorna umbaralho de 52 cartas em ordem aleatória
+from extrai_naipe import extrai_naipe as ex_np # funcionando; args=(carta) retorna o naipe de uma carta
+from extrai_valor import extrai_valor as ex_val # funcionando; args=(carta) retorna o valor numérico de uma carta
+from possiveis_movimentos import lista_movimentos_possiveis as mov_pos # funcionando; args=(baralho, índice) retorna os movimentos possíveis para uma determinada carta
+from empilha import empilha as emp # funcionando mas tá um pouco confuso; args=(baralho, posição inicial, posição de destino), retorna o baralho após o empilhamento de uma carta
+# A função que checa se ainda existem movimentos no baralho retorna False quando implementada agora. Ela será chamada posteriormente.
 
-print("Paciência Acordeão\n")
-# Escrever regras do jogo
+# Agora para iniciar o jogo:
 
-# Iniciando o jogo:
-b_jogo = cria_baralho()
 jogando = False
-iniciar = int(input("Digite \"1\" para iniciar o jogo: "))
-if iniciar == 1: 
+print('PACIÊNCIA ACORDEÃO\n')
+iniciar = int(input("Para iniciar o jogo, digite 1: "))
+
+if iniciar == 1:
     jogando = True
+    b_jogo = cb()
+else:
+    jogando == False
 
 while jogando:
-    # Mostrando as cartas para o jogador:
-    # esse comando exibe as cartas do baralho exatamente na ordem de b_jogo, porém são mostradas com
-    # seu índice e com cores diferentes para uma melhor visualização de seus naipes
-    # '♦', '♥', '♣', '♠'
-    c_col = []
-    for c in b_jogo: # Da a cor de cada naipe
 
-        if "♠" in c: 
-            col = "\033[1;34;40m {0}" # Azul
-            c_col.append(col.format(c))
-        elif "♣" in c:
-            col = "\033[1;35;40m {0}" # Magenta
-            c_col.append(col.format(c))
-        elif "♥" in c: 
-            col = "\033[1;31;40m {0}" # Vermelho
-            c_col.append(col.format(c))
-        elif "♦" in c:
-            col = "\033[1;32;40m {0}" # Verde
-            c_col.append(col.format(c))
+    # Para mostrar uma carta por linha, mostrando seu respectivo índice:
 
-    for a in c_col: # Da o índice de cada carta (comecando em zero)
-
-        indice_da_carta = c_col.index(a)
-        if indice_da_carta < 10:
-            ind = ("\033[0;37;40m {0}".format(indice_da_carta))
-            print(" {0}. ".format(ind) + a)
+    for carta in b_jogo:
+        indice_da_carta = b_jogo.index(carta)
+        if indice_da_carta < 10: 
+            ind = " {0}. ".format(indice_da_carta)
         else:
-            ind = ("\033[0;37;40m {0}".format(indice_da_carta))
-            print("{0}. ".format(ind)+ a)
-    
-    jogando = False
+            ind = "{0}. ".format(indice_da_carta)
+
+        valor_da_carta = ex_val(carta)
+        if valor_da_carta == "10":
+            print(ind + carta)
+        else:
+            print(ind + " " + carta)
+
+
+    escolha = input('Digite o que você deseja fazer:')
+    if escolha != "continuar":
+        jogando = False
+        print("\nFim do Jogo")
+    # O display das cartas no terminal está funcionando normalmente. 
+    # Posteriormente eu vou implementar as cores das cartas de acordo com seu respectivo naipe.
+
+    # Agora a próxima etapa é dar decisões ao jogador:
+    # a função "possui movimentos" será executado. Caso retorne True, o jogo continua. Caso retorne False,
+    # o jogo acaba.
