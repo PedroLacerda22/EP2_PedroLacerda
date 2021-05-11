6e2b7ac0c68b8adc1089c1835dba8d10d84d9249
Pedro Lacerda <pedroagml@al.insper.edu.br>
2021-05-11 16:52:10 -0300

    As cartas com valor 10 não estavam funcionando para serem empilhadas. O problema estava na função de mov_possiveis. Acredito que as mudanças que fiz resolveram o problema.

--- paciencia.py
+++ paciencia.py
@@ -1,27 +1,32 @@
-# Novo plano é tentar fazer o jogo funcionar antes de implementar as coisasque fazem ele ficar bonito.
-
-# Importa as bibliotecas necessárias:
+# Importa as funções necessárias:
 
 from cria_baralho import cria_baralho as cb # funcionando; args=(), retorna um baralho de 52 cartas em ordem aleatória
 from possiveis_movimentos import lista_movimentos_possiveis as mov_pos # funcionando; args=(baralho, índice) retorna os movimentos possíveis para uma determinada carta
-from empilha import empilha as emp # funcionando mas tá um pouco confuso; args=(baralho, posição inicial, posição de destino), retorna o baralho após o empilhamento de uma carta
-from mostra_baralho import mostra_cartas as mc # status? args=(baralho), mostra o baralho no terminal
-# A função que checa se ainda existem movimentos no baralho retorna False quando implementada agora. Ela será chamada posteriormente.
+from empilha import empilha as emp # funcionando; args=(baralho, posição inicial, posição de destino), retorna o baralho após o empilhamento de uma carta
+from mostra_baralho import mostra_cartas as mc # funionando; args=(baralho), mostra o baralho no terminal
 
 # Agora para iniciar o jogo:
-
 tem_movimentos = True
 jogando = False
-print('PACIÊNCIA ACORDEÃO\n')
+print('Bem vindo ao jogo de Paciência Acordeão\n')
+
+print("As regras do jogo são as seguintes:")
+print("As cartas só podem der movimentadas para duas posições:")
+print("- Seu 1º vizinho anterior ou\n- Seu 3º vizinho anterior\n")
+print("O movimento das cartas só é permitido se as duas cartas:")
+print("- São do mesmo naipe ou\n- Têm o mesmo valor numérico\n")
+print("O jogador vence quando resta apenas uma carta no jogo.")
+print("Caso não existam movimentos possíveis ou o jogador digitar zero (0) como o índice da carta, o jogo é encerrado.\n")
+
 iniciar = int(input("Para iniciar o jogo, digite 1: "))
 
 if iniciar == 1:
     jogando = True
     b_jogo = cb()
 else:
-    jogando == False
+    jogando = False
 
-from possui_movimentos import possui_movimentos_possiveis as t_mov # status?
+from possui_movimentos import possui_movimentos_possiveis as t_mov # funcionando; args=(baralho)
 while jogando:
     
     # Define a condição de fim de jogo
@@ -29,6 +34,10 @@ while jogando:
     if not tem_movimentos:
         jogando = False
         print('Sem movimentos restantes. Fim do jogo.')
+    
+    if len(b_jogo) == 1:
+        jogando = False
+        print("Você venceu!")
     # Para mostrar uma carta por linha, mostrando seu respectivo índice:
 
     print(mc(b_jogo))
@@ -42,17 +51,17 @@ while jogando:
             jogando = False
             print("\nFim do Jogo")
         
-        elif indice_carta in range(0, len(b_jogo)+1):
+        elif indice_carta in range(0, len(b_jogo)):
             # Selecionou um índice
             indice_carta_mov = mov_pos(b_jogo, indice_carta)
 
             if len(indice_carta_mov) == 0: # funcionando
-                # Se for a primeira carta, não da pra mover
+                # Se for a primeira carta, não da pra 
+                print(mc(b_jogo))
                 print('Não é possivel mover essa carta.')
-
+                
             elif len(indice_carta_mov) == 1:
                 # Se a função mov_pos retornou uma lista com só um item:
-
                 if indice_carta_mov[0] == 1:
                     # Se só é possível mover a carta para o vizinho anterior
                     b_novo = emp(b_jogo, indice_carta, b_jogo.index(b_jogo[indice_carta]) - 1)
@@ -76,7 +85,7 @@ while jogando:
                 # O jogador pode escolher em qual vizinho a cara será empilhada.
 
                 if qual_emp == 1:
-                    b_novo = emp(b_jogo, b_jogo.index(indice_carta), b_jogo.index(b_jogo[indice_carta]) - 1)
+                    b_novo = emp(b_jogo, indice_carta, b_jogo.index(b_jogo[indice_carta]) - 1)
                     b_jogo = b_novo
                     print(mc(b_jogo))
                     print("A carta foi empilhada em seu vizinho anterior")
@@ -89,8 +98,7 @@ while jogando:
 
                     # talvez de pra encurtar esses if elif statements.
                 else:
+                    print(mc(b_jogo))
                     print("Entrada invalida.")
 
-    # Por enquanto, a continuidade do jogo está funcionando bem. Mas é preciso implemenetar uma função que mostra o baralho (1 carta por linha)
-    # de modo com que possa ser chamada após o fim de cada uma das decisões.
-    # Atualmente, ao final da primeira decisão o código imprime a lista de cartas ao invés de cada uma por linha juntamente ao seus índices.
+    # o stacking não esá funcionando para empilhar uma carta sobre uma outra carta de valor 10. A operação funciona normalmente no outro sentido
--- possiveis_movimentos.py
+++ possiveis_movimentos.py
@@ -1,61 +1,37 @@
 def lista_movimentos_possiveis(baralho, index):
-    c_sel = baralho[index]
+    c_sel = baralho[index] # carta selecionada do baralho
+    val_carta = ex_val(c_sel)
+    v1 = ex_val(baralho[index - 1])
+    v3 = ex_val(baralho[index - 3])
+    np_carta = ex_np(c_sel)
+    n1 = ex_np(baralho[index - 1])
+    n3 = ex_np(baralho[index - 3])
 
-    if index == 0:
+    if index == 0: # se o indice da carta é igual a zero, não há movimentos possíveis
         return []
-    elif index <= 2:
-        if len(c_sel) == 3:
-            if "10" in baralho[index - 1]:
-                # carta anterior é 10
-                return [1]
-            elif baralho[index - 1][-1] == c_sel[-1]:
-                # msm naipe da anterior
-                return [1]
-            else:
-                return []
 
-        if baralho[index - 1][0] == c_sel[0]:
-            # msm valor
+    elif index <= 2: # para as 3 primeiras cartas do baralho
+        if val_carta == v1:
             return [1]
-        elif baralho[index - 1][1] == c_sel[1]:
-            # msm naipe
+        elif np_carta == n1:
             return [1]
         else:
             return []
 
     elif index > 2:
         p_mov = []
-        if len(c_sel) == 3:
-            if "10" in baralho[index - 1]:
-                # msm valor 1
-                p_mov.append(1)
-            elif baralho[index - 1][-1] == c_sel[-1]:
-                # msm naipe 1
-                p_mov.append(1)
-
-            if "10" in baralho[index - 3]:
-                # msm valor 3
-                p_mov.append(3)
-            elif baralho[index - 3][-1] == c_sel[-1]:
-                # msm naipe 3
-                p_mov.append(3)
-
-            return p_mov
-
-        elif baralho[index - 1][0] == c_sel[0]:
+        if val_carta == v1:
             # msm valor 1
             p_mov.append(1)
-        elif baralho[index - 1][1] == c_sel[1]:
+        elif np_carta == n1:
             # msm naipe 1
             p_mov.append(1)
-
-        if baralho[index - 3][0] == c_sel[0]:
+        
+        if val_carta == v3:
             # msm valor 3
             p_mov.append(3)
-        elif baralho[index - 3][1] == c_sel[1]:
-            # ms naipe 3
+        elif np_carta == n3:
+            # msm naipe 3
             p_mov.append(3)
-        else: 
-            return p_mov
 
         return p_mov
